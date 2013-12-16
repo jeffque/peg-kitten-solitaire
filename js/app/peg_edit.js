e4f6a4628f5cc5950567649858cfb5fa9aa5dd18
Jefferson Quesado <jeff.quesado@gmail.com>
2013-12-16 14:30:44 -0300

    Adding and removing rows flawlessly!

--- js/app/peg_edit.js
+++ js/app/peg_edit.js
@@ -24,9 +24,29 @@ function clicked(cell) {
 }
 
 function rows_minus() {
+	var code_obj = new PK.peg_code(window.location.hash);
+
+	code_obj.rows--;
+
+	code_obj.code = code_obj.code.slice(0, code_obj.rows * code_obj.columns);
+	console.debug('novo code após rows_minus: ' + code_obj.stringfy());
+
+	remove_gameboard();
+	decodify_gameboard(code_obj.stringfy());
 }
 
 function rows_plus() {
+	var code_obj = new PK.peg_code(window.location.hash);
+	var i = '';
+
+	code_obj.rows++;
+
+	for (i = 0; i < code_obj.columns; i++) {
+		code_obj.code += 'I';
+	}
+
+	remove_gameboard();
+	decodify_gameboard(code_obj.stringfy());
 }
 
 function columns_minus() {
